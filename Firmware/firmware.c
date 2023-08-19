872bae72f36cbf045ebcebbdb42734890220c18e
Andrew Howard <mannndrew@gmail.com>
2023-08-18 22:23:43 -0500

    Nearly finished firmware

--- Firmware/firmware.c
+++ Firmware/firmware.c
@@ -19,27 +19,10 @@ void *memcpy(void *dest, const void *src, int n)
 
 int main()
 {
-	char message[] = "$Uryyb+Jbeyq!+Vs+lbh+pna+ernq+guvf+zrffntr+gura$gur+CvpbEI32+PCH"
-			"+frrzf+gb+or+jbexvat+whfg+svar.$$++++++++++++++++GRFG+CNFFRQ!$$";
-	for (int i = 0; message[i]; i++)
-		switch (message[i])
-		{
-		case 'a' ... 'm':
-		case 'A' ... 'M':
-			message[i] += 13;
-			break;
-		case 'n' ... 'z':
-		case 'N' ... 'Z':
-			message[i] -= 13;
-			break;
-		case '$':
-			message[i] = '\n';
-			break;
-		case '+':
-			message[i] = ' ';
-			break;
-		}
-	puts(message);
-	
+	int value = 15;
+	int *memory_location = (int *)0x400;
+
+	*memory_location = value;
+
 	return 0;
 }
---
+++ Firmware/makehex.py
@@ -0,0 +1,76 @@
+import sys
+
+memory = []
+width = 32
+depth = 65536
+
+# Check if the correct number of arguments were provided
+if len(sys.argv) != 2:
+    print("Usage: python makehex.py <file>")
+    sys.exit(1)
+
+# Get the file name from the arguments
+file_name = sys.argv[1]
+
+# Check if the file name is valid
+if file_name[-4:] != ".bin":
+    print("Error: File must be '.bin'")
+    sys.exit(1)
+
+# Check if file exists and open it
+binary_file = None
+try:
+    binary_file = open(file_name, "rb")
+except FileNotFoundError:
+    print(f"Error: File '{file_name}' not found")
+    sys.exit(1)
+
+# Read the contents of the file
+binary_data = binary_file.read()
+
+# Display the binary data as hexadecimal
+for byte in binary_data:
+    byte = hex(byte)[2:]
+    if len(byte) == 1:
+        byte = "0" + byte
+    byte = str.upper(byte)
+    memory.append(byte)
+
+# Close the binary file
+binary_file.close()
+
+
+
+
+# Open the output file in write mode
+file_path = file_name[:-4] + ".mif"
+output_file = open(file_path, "w")
+
+# Write the MIF header
+output_file.write(f"DEPTH = {depth};\n")
+output_file.write(f"WIDTH = {width};\n")
+output_file.write(f"ADDRESS_RADIX = HEX;\n")
+output_file.write(f"DATA_RADIX = HEX;\n")
+output_file.write(f"CONTENT\n")
+output_file.write(f"BEGIN\n")
+
+# Write the contents of the memory
+byte_count = 0
+for address in range(depth):
+    word = ""
+
+    if byte_count < len(memory):
+        for byte in range(width // 8):
+            word = word + memory[byte_count]
+            byte_count += 1
+
+    else:
+        word = "00000000"
+
+    output_file.write(f"{address:04x}: {word};\n")
+
+# Write the MIF footer
+output_file.write(f"END;")
+
+# Close the output file
+output_file.close()
