7d87742c7a18d065bd04ebe3b6ed8b9f622d1ce7
Andrew Howard <mannndrew@gmail.com>
2024-01-28 19:45:25 -0600

    Modified program

--- Firmware/firmware.c
+++ Firmware/firmware.c
@@ -26,48 +26,31 @@
 
 // 	return 0;
 // }
-#include <stdint.h>
 
-#define LED     (*((volatile uint32_t *)0x0))
+#include <stdint.h>
+#include <stdbool.h>
+
+#define LED     (*((volatile uint32_t *)0x800))
+
+volatile uint32_t word = 10;
 
 void wait3Seconds(void)
 {
-    __asm("counter: .word 30000000");
-    __asm("          LUI   a0, %hi(counter)"); // 2
-    __asm("          LW    a0, %lo(counter)(a0)"); // 2
-    
-
-    __asm("loop:                    ");
+    __asm("          LW    a0, word"); // 2
     __asm("          ADDI  a0, a0, -1"); // 1
-    __asm("          BEQZ  a0, done"); // 1
-    __asm("          JAL   x0, loop"); // 2
-    __asm("done:                    "); // 4 Total * Counter
+    __asm("          BEQZ  a0, 4"); // 1
+    return;
+}
+
+void toggleLED(void)
+{
+    LED = 1;
+    return;
 }
 
 void main() {
-    
-    int i;
-    static const int arr[10] = {9, 28, 53, 88, 52, 9, 75, 6, 26, 7};
-    int maxNumber = arr[0];
-
     wait3Seconds();
+    toggleLED();
 
-    for(i = 1;i < 10; i++)
-    {
-       if(maxNumber < arr[i])
-           maxNumber = arr[i] ;
-    }
-
-    volatile char *tx1 = (volatile char *)0x40002000;
-    const char *result = "The largest element is ";
-    while(*result)
-    {
-        *tx1 = *result;
-        result++;
-    }
-
-
-    *tx1 = maxNumber;
-
-    return;
+    while(true);
 }
